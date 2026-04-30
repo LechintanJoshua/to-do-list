827a31e8334538c271e9c5c92f33263674f1e8d2
LechintanJoshua <jlechintan79@gmail.com>
2026-04-30 20:10:06 +0300

    Add Fonts to project
    Install webpack dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,19 @@ module.exports =  {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
-            }
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            }, 
+            {
+                test: /\.(png|svg|jpg)$/i,  
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
