4981dd3dde12c22785423f90c88fe8f8bfb399e8
LechintanJoshua <jlechintan79@gmail.com>
2026-06-15 17:47:19 +0300

    Finish taskDetailController structure

--- src/scripts/menuController.js
+++ src/scripts/menuController.js
@@ -8,7 +8,6 @@ export default class MenuController {
     #list;
     #tags;
     #addList;
-    #addTag;
     #settings;
     #signOut;
     
@@ -28,11 +27,9 @@ export default class MenuController {
         this.#addList = document.querySelector('.add-list');
         this.#settings = document.querySelector('.settings');
         this.#signOut = document.querySelector('.sign-out');
-        this.#addTag = document.querySelector('.tag-btn');
 
         this.#search.addEventListener('input', () => this.#searchTask());      
         this.#addList.addEventListener('click', () => this.#createList());
-        this.#addTag.addEventListener('click', () => this.#createTag());
         this.#tags.addEventListener('click', () => this.#checkTags());
         this.#topIcon.addEventListener('click', () => alert("To be implemented"));
         this.#settings.addEventListener('click', () => alert("To be implemented"));
@@ -52,6 +49,6 @@ export default class MenuController {
     }
 
     #checkTags() {
-        
+
     }
 }
---
+++ src/scripts/taskDetailController.js
@@ -0,0 +1,47 @@
+import { addTagPanels } from "./adder.js";
+
+export default class TaskDetailController {
+    #name;
+    #description;
+    #list;
+    #dueDate;
+    #addTag;
+    #addSubtask;
+    #tagsCont;
+    #saveBtn;
+    #delBtn;
+
+    constructor() {
+        this.#initListeners();
+    }
+
+    #initListeners() {
+        this.#name = document.querySelector('#task-title');
+        this.#description = document.querySelector('#description');
+        this.#list = document.querySelector('#list-fam');
+        this.#dueDate = document.querySelector('#due-date');
+        this.#addTag = document.querySelector('.add-tag-btn');
+        this.#tagsCont = document.querySelector('.but-opt')
+        this.#addSubtask = document.querySelector('.add-sub');
+        this.#saveBtn = document.querySelector('.btn-delete');
+        this.#delBtn = document.querySelector('.btn-save');
+
+        this.#addTag.addEventListener('click', () => this.#takeTagInfo());
+        this.#addSubtask.addEventListener('click', () => this.#takeSubInfo());
+        this.#tagsCont.addEventListener('click', (e) => this.#checkTagClick(e));
+        this.#saveBtn.addEventListener('click', () => this.#upsertTask());
+        this.#delBtn.addEventListener('click', () => this.#removeTask());
+    }
+
+    #takesubInfo () {
+
+    }
+
+    #takeTagInfo() {
+
+    }
+
+    #checkTagClick(e) {
+
+    }
+}
